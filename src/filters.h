38176f49467a9b6788158e427bb393495e19ab71
lshao <laetitia.shao@gmail.com>
2017-02-15 14:29:30 +0100

    Filter functions on one image

--- src/filters.h
+++ src/filters.h
@@ -3,3 +3,7 @@
 void apply_blur_filter( animated_gif * image, int size, int threshold );
 void apply_gray_filter( animated_gif * image );
 void apply_sobel_filter( animated_gif * image );
+
+void apply_gray_filter_once(pixel *oneImage, int size);
+void apply_blur_filter_once(pixel* oneImage, int width, int height, int blurSize, int threshold);
+void apply_sobel_filter_once(pixel *oneImage, int width, int height);
